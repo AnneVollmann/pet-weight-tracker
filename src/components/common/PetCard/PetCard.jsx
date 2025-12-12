28d01c57fb4119f9a85ae73b23113689e80d759b
Anne Vollmann <vollmann.anne@web.de>
2025-12-12 10:18:28 +0100

    feat: pet-detail-page fetches pet

--- src/components/common/PetCard/PetCard.jsx
+++ src/components/common/PetCard/PetCard.jsx
@@ -1,16 +1,16 @@
 import "./PetCard.css"
 import { Link } from "react-router-dom";
 
-export default function PetCard({id, petName, img }) {
+export default function PetCard({ id, petName, img }) {
   return (
     <div className="pet-card card" style={{ width: '18rem' }}>
-      <img src={"/assets/img/pet-" + img + ".png"} className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">{petName}</h5>
-        <Link to={`/pet/${id}`}>
+      <Link to={`/pet/${id}`} className="pet-card-link">
+        <img src={"/assets/img/pet-" + img + ".png"} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{petName}</h5>
           <button type="button" className="btn btn-add" />
-        </Link>
-      </div>
+        </div>
+      </Link>
     </div>
   );
 }
--- src/pages/PetDetailPage/PetDetailPage.jsx
+++ src/pages/PetDetailPage/PetDetailPage.jsx
@@ -1,9 +1,24 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getPetById } from "../../firebase/pets";
 
 export default function PetDetailPage() {
     const { id } = useParams();
+    const [pet, setPet] = useState(null);
+
+    useEffect(() => {
+        async function fetchPet() {
+            const p = await getPetById(id);
+            setPet(p);
+        }
+        fetchPet();
+    }, [id]);
+
+    if (!pet) return <h1>Lade...</h1>;
 
     return (
-        <div>Pet Detail for ID: {id}</div>
+        <section>
+            <h1>{pet.name}</h1>
+        </section>
     );
 }
