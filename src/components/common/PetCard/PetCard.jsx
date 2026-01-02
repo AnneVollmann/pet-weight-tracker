521f8ad83f90dbba7d9f05826f60359bccecdb14
Anne Vollmann <vollmann.anne@web.de>
2026-01-02 14:09:36 +0100

    feat: prevented event-bubbling for btn-add in PetCard while the Link still works

--- src/components/common/PetCard/PetCard.jsx
+++ src/components/common/PetCard/PetCard.jsx
@@ -4,19 +4,20 @@ import { Link } from "react-router-dom";
 export default function PetCard({ pet, onAddWeight }) {
   return (
     <div className="pet-card card" style={{ width: '16rem' }}>
-      {/* <Link to={`/pet/${id}`} className="pet-card-link"> */}
+      <Link to={`/pet/${pet.id}`} className="pet-card-link">
         <img src={"/assets/img/pet-" + pet.img + ".png"} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{pet.name}</h5>
           <button
             onClick={(e) => {
+              e.preventDefault();
               e.stopPropagation();
               onAddWeight(pet);
             }}
             type="button"
             className="btn btn-add" />
         </div>
-      {/* </Link> */}
+      </Link>
     </div>
   );
 }
