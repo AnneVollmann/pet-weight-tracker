4de20a6cded402bc2fe8b568c0afec4c38477d33
Anne Vollmann <vollmann.anne@web.de>
2026-01-05 12:38:37 +0100

    refactor: nav-elements overhauled

--- src/components/ui/Navbar/Navbar.jsx
+++ src/components/ui/Navbar/Navbar.jsx
@@ -5,9 +5,15 @@ export default function Navbar() {
     return (
         <nav className="navbar bg-body-tertiary">
             <div className="container-fluid">
-                <Link to="/"><button type="button" className="btn-nav btn-home btn" /></Link>
-                <Link to="/weightentry"><button type="button" className="btn-nav btn-new-entry btn" /></Link>
-                <Link to="/settings"><button type="button" className="btn-nav btn-settings btn" /></Link>
+                <Link to="/">
+                    <button type="button" className="btn-nav btn-home btn" />
+                </Link>
+                <Link to="/weightentry">
+                    <button type="button" className="btn-nav btn-new-entry btn" />
+                </Link>
+                <Link to="/settings">
+                    <button type="button" className="btn-nav btn-settings btn" />
+                </Link>
             </div>
         </nav>
     );
