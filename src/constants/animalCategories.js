2d6068e26d1066de8a4181a4ddcb60c4420e20f8
Anne Vollmann <vollmann.anne@web.de>
2026-04-16 12:47:21 +0200

    feat: icons for animalCategories

--- src/constants/animalCategories.js
+++ src/constants/animalCategories.js
@@ -1,4 +1,4 @@
 export const animalCategories = [
-    {id: "guinea-pig", label: "Meerschweinchen", img:""},
-    {id: "cat", label: "Katze", img:""}
+    {id: "guinea-pig", label: "Meerschweinchen", img:"./assets/icons/guinea-pig.svg"},
+    {id: "cat", label: "Katze", img:"./assets/icons/cat.svg"}
 ]
