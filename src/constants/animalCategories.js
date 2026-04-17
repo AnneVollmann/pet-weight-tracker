cc8ea2375d3c2ab750f291d49fe6f5a8efb93cd8
Anne Vollmann <vollmann.anne@web.de>
2026-04-17 13:13:53 +0200

    style: animal category icons css

--- src/constants/animalCategories.js
+++ src/constants/animalCategories.js
@@ -1,4 +1,4 @@
 export const animalCategories = [
-    {id: "guinea-pig", label: "Meerschweinchen", img:"./assets/icons/guinea-pig.svg"},
-    {id: "cat", label: "Katze", img:"./assets/icons/cat.svg"}
+    {id: "guinea-pig", label: "Meerschweinchen"},
+    {id: "cat", label: "Katze"}
 ]
--- src/pages/HomePage/CategorySelector/CategorySelector.jsx
+++ src/pages/HomePage/CategorySelector/CategorySelector.jsx
@@ -7,14 +7,14 @@ export default function CategorySelector({ selectedSpecies, onSelectSpecies }) {
             <a
                 className={`nav-link ${selectedSpecies === null ? "active" : ""}`}
                 onClick={() => onSelectSpecies(null)}>
-                Alle Tiere
+                Alle
             </a>
             {animalCategories.map((category) => (
                 <a
                     className={`nav-link ${selectedSpecies === category.id ? "active" : ""}`}
                     key={category.id}
                     onClick={() => onSelectSpecies(category.id)}>
-                    {category.label}
+                    <img src={`./assets/icons/${category.id}${selectedSpecies === category.id ? "-light" : ""}.svg`} alt={category.id + "-icon"} className="animal-icon" />
                 </a>
             ))}
         </nav>
