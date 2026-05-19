a1263d8569740aaea275df7c094728763d4ae9f5
Anne Vollmann <vollmann.anne@web.de>
2026-05-19 11:42:42 +0200

    refactor: PetDetailPage overhauled

--- src/pages/PetDetailPage/PetDetailPage.jsx
+++ src/pages/PetDetailPage/PetDetailPage.jsx
@@ -11,6 +11,7 @@ import WarningBadge from "./components/WarningBadge/WarningBadge";
 import BasicModal from "../../components/ui/BasicModal/BasicModal";
 import PetDetails from "./components/PetDetails/PetDetails";
 import { formatDateToString } from "../../lib/dates/formatDate";
+import { animalCategories } from "../../constants/animalCategories";
 
 export default function PetDetailPage() {
     const { id } = useParams();
@@ -60,37 +61,52 @@ export default function PetDetailPage() {
         return acc;
     }, {});
 
+    function getPetSpeciesLabel(species) {
+        const category = animalCategories.find(category => category.id === species)
+        return category.label
+    }
+
     if (!pet) return <h1>Lade...</h1>;
 
     return (
         <section className="page pet-detail-page">
-
-            <div className="pet-detail-overview">
-                <header>
+            <div className="pet-detail-header">
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
                     <Link to="/">
                         <button type="button" className="btn-back btn" />
                     </Link>
-                    <h1>{pet.name}</h1>
                     <button onClick={(e) => {
                         e.preventDefault();
                         e.stopPropagation();
                         setShowPetDetails(true);
-                    }} type="button" className="btn-infos btn"/>
-                </header>
-                {showWeightWarning && <WarningBadge petName={pet.name} ></WarningBadge>}
-                {weights.length >= 2 && <p className="pet-average-weight">Durchschnittsgewicht: {periodAverageWeightValue} g </p>}
-                {weights.length === 0 && <p className="no-entries">Noch keine Einträge</p>}
+                    }} type="button" className="btn-infos btn" />
+                </div>
+
+                <div className="overview">
+                    <img className="pet-img" src={"/assets/img/" + pet.species + "-" + pet.name.toLowerCase() + ".png"} />
+                    <div className="overview-information">
+                        <h1>{pet.name}</h1>
+                        <p>{getPetSpeciesLabel(pet.species)}
+                            {pet.sex ? ", " + pet.sex : ""}
+                        </p>
+                        <p>{pet.birthday ? "geboren " + pet.birthday : ""}</p>
+                    </div>
+                    {/* {showWeightWarning && <WarningBadge petName={pet.name} ></WarningBadge>}
+                        {weights.length >= 2 && <p className="pet-average-weight">Durchschnittsgewicht: {periodAverageWeightValue} g </p>}*/}
+                </div>
             </div>
 
-            <div className="weight-chart">
-                <WeightChart
-                    sortedWeights={sortedWeights}
-                    visibleWeights={visibleWeights}
-                    weightWarningShown={showWeightWarning}
-                    onSelectWeight={setSelectedWeight}
-                />
+            {weights.length > 0 ?
+                <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+                    <div className="weight-chart module">
+                        <WeightChart
+                            sortedWeights={sortedWeights}
+                            visibleWeights={visibleWeights}
+                            weightWarningShown={showWeightWarning}
+                            onSelectWeight={setSelectedWeight}
+                        />
 
-                <p className="selected-weight">
+                        {/* <p className="selected-weight">
                     {selectedWeight === null ? (
                         "\u00A0"
                     ) : (
@@ -99,13 +115,18 @@ export default function PetDetailPage() {
                             {selectedWeight.weight} g
                         </>
                     )}
-                </p>
-            </div>
+                </p> */}
+                    </div>
 
-            <WeightTable
-                groupedWeights={groupedWeights}
-                onSelectPeriodEndDate={setPeriodEndDate}
-            />
+                    <div className="module">
+                        <WeightTable
+                            groupedWeights={groupedWeights}
+                            onSelectPeriodEndDate={setPeriodEndDate}
+                        />
+                    </div>
+                </div>
+                : <p className="no-entries">Noch keine Einträge</p>
+            }
 
             <BasicModal
                 show={showPetDetails}
@@ -113,8 +134,8 @@ export default function PetDetailPage() {
                 pet={pet.name}
             >
                 <PetDetails
-                pet={pet}
-                onUpdatePet={setPet}>
+                    pet={pet}
+                    onUpdatePet={setPet}>
                 </PetDetails>
             </BasicModal>
         </section>
