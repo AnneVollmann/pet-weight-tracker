9d110e67fbc230cbd04d5a514caa3f2eb16a7c30
Anne Vollmann <vollmann.anne@web.de>
2026-05-28 12:30:01 +0200

    style: pet-card design adjusted for pets without img

--- src/pages/HomePage/PetCard/PetCard.jsx
+++ src/pages/HomePage/PetCard/PetCard.jsx
@@ -5,9 +5,11 @@ export default function PetCard({ pet, onAddWeight }) {
   return (
     <div className="pet-card card">
       <Link to={`/pet/${pet.id}`} className="pet-card-link">
-        <img src={"/assets/img/" + pet.species + "-" + pet.name.toLowerCase() + ".png"}
-          className="card-img-top"
-          alt={pet.species + "-" + pet.name.toLowerCase()} />
+        <div style={{height: "calc(100% - 64px)"}}>
+          <img src={"/assets/img/" + pet.species + "-" + pet.name.toLowerCase() + ".png"}
+            className="card-img-top"
+            alt={pet.species + "-" + pet.name.toLowerCase()} />
+        </div>
         <div className="card-body">
           <h5 className="card-title">{pet.name}</h5>
           <button
